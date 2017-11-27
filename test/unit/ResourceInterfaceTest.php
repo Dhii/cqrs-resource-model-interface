054cabd288deb30d3e088fd808755e3d0c44d257
Miguel Muscat <miguelmuscat93@gmail.com>
2017-11-27 17:23:11 +0100

    Added test for `ResourceInterface`

---
+++ test/unit/ResourceInterfaceTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Dhii\Storage\Resource\UnitTest;
+
+use Xpmock\TestCase;
+use Dhii\Storage\Resource\ResourceInterface as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class ResourceInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Storage\Resource\ResourceInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                     ->select()
+                     ->insert()
+                     ->update()
+                     ->delete();
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+    }
+}
