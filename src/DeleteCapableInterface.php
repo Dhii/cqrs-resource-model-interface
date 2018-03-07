636b10cc63f2916cbbb36174ae712b923522de27
Miguel Muscat <miguelmuscat93@gmail.com>
2018-03-07 17:27:12 +0100

    Added ordering, limiting and offsetting params ...
    ... in `DeleteCapableInterface`

--- src/DeleteCapableInterface.php
+++ src/DeleteCapableInterface.php
@@ -3,6 +3,8 @@
 namespace Dhii\Storage\Resource;
 
 use Dhii\Expression\LogicalExpressionInterface;
+use Dhii\Storage\Resource\Sql\OrderInterface;
+use Traversable;
 
 /**
  * Something that can delete records from storage.
@@ -16,10 +18,18 @@ interface DeleteCapableInterface
      *
      * @since [*next-version*]
      *
-     * @param LogicalExpressionInterface|null $condition An optional condition which, if specified, restricts the
-     *                                                   deletion to records that satisfy this condition.
+     * @param LogicalExpressionInterface|null   $condition An optional condition which, if specified, restricts the
+     *                                                     deletion to records that satisfy this condition.
+     * @param OrderInterface[]|Traversable|null $ordering  The ordering, as a list of OrderInterface instances.
+     * @param int|null                          $limit     The number of records to limit the query to.
+     * @param int|null                          $offset    The number of records to offset by, zero-based.
      *
      * @return int The number of affected rows.
      */
-    public function delete(LogicalExpressionInterface $condition = null);
+    public function delete(
+        LogicalExpressionInterface $condition = null,
+        $ordering = null,
+        $limit = null,
+        $offset = null
+    );
 }
