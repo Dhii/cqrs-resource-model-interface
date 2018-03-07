0ad59655aad16a3fa71df8333eaa2018284b56f6
Miguel Muscat <miguelmuscat93@gmail.com>
2018-03-07 17:27:28 +0100

    Added ordering and limiting in ...
    ... `UpdateCapableInterface`

--- src/UpdateCapableInterface.php
+++ src/UpdateCapableInterface.php
@@ -2,6 +2,7 @@
 
 namespace Dhii\Storage\Resource;
 
+use Dhii\Storage\Resource\Sql\OrderInterface;
 use Dhii\Util\String\StringableInterface as Stringable;
 use Dhii\Expression\LogicalExpressionInterface;
 use Dhii\Expression\TermInterface;
@@ -24,8 +25,10 @@ interface UpdateCapableInterface
      *                                                                                                 The values don't have to be all of the same type.
      * @param LogicalExpressionInterface|null                                               $condition An optional condition which, if specified, restricts the
      *                                                                                                 affected records to those that satisfy this condition.
+     * @param OrderInterface[]|Traversable|null                                             $ordering  The ordering, as a list of OrderInterface instances.
+     * @param int|null                                                                      $limit     The number of records to limit the query to.
      *
      * @return int The number of affected records.
      */
-    public function update($changeSet, LogicalExpressionInterface $condition = null);
+    public function update($changeSet, LogicalExpressionInterface $condition = null, $ordering = null, $limit = null);
 }
