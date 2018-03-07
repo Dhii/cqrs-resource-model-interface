8be82c828ab73c0e02182d10cd71c917c42bb22c
Miguel Muscat <miguelmuscat93@gmail.com>
2018-03-07 17:26:29 +0100

    Added ordering, limiting and offsetting params ...
    ... in `SelectCapableInterface`

--- src/SelectCapableInterface.php
+++ src/SelectCapableInterface.php
@@ -3,6 +3,7 @@
 namespace Dhii\Storage\Resource;
 
 use Dhii\Expression\LogicalExpressionInterface;
+use Dhii\Storage\Resource\Sql\OrderInterface;
 use Psr\Container\ContainerInterface;
 use Traversable;
 
@@ -17,12 +18,20 @@ interface SelectCapableInterface
      * Retrieves records from storage.
      *
      * @since [*next-version*]
-     * @see ContainerInterface
+     * @see   ContainerInterface
      *
-     * @param LogicalExpressionInterface|null $condition An optional condition which, if given, restricts the result
-     *                                                   set to records that satisfy this condition.
+     * @param LogicalExpressionInterface|null   $condition An optional condition which, if given, restricts the result
+     *                                                     set to records that satisfy this condition.
+     * @param OrderInterface[]|Traversable|null $ordering  The ordering, as a list of OrderInterface instances.
+     * @param int|null                          $limit     The number of records to limit the query to.
+     * @param int|null                          $offset    The number of records to offset by, zero-based.
      *
      * @return ContainerInterface[]|Traversable A list of containers, each containing the data for a single record.
      */
-    public function select(LogicalExpressionInterface $condition = null);
+    public function select(
+        LogicalExpressionInterface $condition = null,
+        $ordering = null,
+        $limit = null,
+        $offset = null
+    );
 }
